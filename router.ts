4a2cc3471a3913cbabd293468073fc3c1cad4e5e
mguomanila <12101668+mguomanila@users.noreply.github.com>
2021-08-30 23:26:39 +0800

    fix lockerusertype uri

--- router.ts
+++ router.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, LockerTimeLimit } from '@prisma/client'
+import { PrismaClient, LockerTimeLimit, LockerUserType } from '@prisma/client'
 import { Router } from 'express'
 
 const prisma = new PrismaClient
@@ -88,6 +88,61 @@ router.post('/lockertimelimit/:id', async (req, res, next) => {
 		if(query){
 			status = true
 			description = 'ok'
+		} else {
+			status = false
+			description = 'error'
+		}
+		res.json({ status, description, })
+	} catch (e) { 
+		console.log(e) 
+		res.status(202)
+	}
+})
+router.get('/lockerusertypes/:id', async (req, res, next) => {
+	const { id } = req.params
+	console.log('id', id)
+	async function main(){
+		return await prisma.lockerUserType.findMany({
+			where: {userIds: parseInt(id)},
+		})
+	}
+	try{
+		const q = await main()
+		if(!q){
+			res.status(202).send('error')
+		} else {
+			res.json(q)
+		}
+	} catch(e){
+		console.log(e)
+		res.status(202)
+	}
+})
+router.post('/lockerusertypes', async (req, res, next) => {
+	const data: LockerUserType[]  = req.body.data || req.body
+
+	const { lockerUserType } = prisma
+	const query: number[] = []
+	async function main(){
+		await Promise.all(
+			data.map(async (lockerType) => {
+				const { id }: {id: number} = lockerType
+				const qr = await lockerUserType.upsert({
+					where: { id: parseInt(`${id}`) },
+					update: lockerType,
+					create: lockerType,
+					select: { id: true }
+				})
+				query.push(qr.id)
+			})
+		)
+	}
+	try{
+		let status, description
+		await main()
+		if(query.length){
+			status = true
+			description = `saved ${query.length} items`
 		} else {
 			status = false
 			description = 'error'
